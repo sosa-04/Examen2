4051ae7c3f673e9f46dccfb71a8b310e4bc56313
sosa-04 <josue.sosaaleman@gmail.com>
2024-04-05 09:55:24 -0600

    first commit

---
+++ src/main/java/hn/unah/lenguajes1900/examen/ExamenApplication.java
@@ -0,0 +1,13 @@
+package hn.unah.lenguajes1900.examen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenApplication.class, args);
+	}
+
+}
